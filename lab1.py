d1b78febbd0dc69cc395b451b95a5e73cb5f30c6
amgharhassina23-ux <amgharhassina23@gmail.com>
2026-06-23 01:41:25 -0400

    Update greeting messege to include lab requirements

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 print("Hello, World!")
 print("Welcome to Python programming.")
 name =input("What is your name?")
-name = input("Hello " + name + ", nice to meet you you!")
+name = input("Hello " + name + ", Glad to have you learning Python.")
