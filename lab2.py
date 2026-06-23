9f6d390571c04d1c4a464a1c7a700359a9eb5c92
amgharhassina23-ux <amgharhassina23@gmail.com>
2026-06-23 16:47:07 -0400

    error and completed lab2

---
+++ lab2.py
@@ -0,0 +1,19 @@
+name = "Hassina"
+age = 40
+height = 1.63
+age_in_5_years = age + 5
+print(f"Hello, my name is {name}, I am {40} years old and {height} meters tall.")
+print(f"In 5 years, Iwill be {age_in_5_years}years old.")
+width = 5.5
+rect_height = 2
+area = width * rect_height
+print(f"The area of a 5.5 * 2 rectangle is {area}.")
+result_div = 10 / 2
+result_mod =10 % 3
+greeting = "Hello " + "World"
+repeat_str = "Hi! " * 3
+print(f"Division: {result_div}, Modulo: {result_mod}")
+print(f"Concatenation: {greeting}")
+print(f"Repetition: {repeat_str}")
+
+      
