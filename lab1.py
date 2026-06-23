b56b78d2dfcc08ef5c7acfc9ef54ea82e38ff416
amgharhassina23-ux <amgharhassina23@gmail.com>
2026-06-23 12:39:57 -0400

    Fixed lab1 code

--- lab1.py
+++ lab1.py
@@ -1,4 +1,4 @@
 print("Hello, World!")
 print("Welcome to Python programming.")
 name =input("What is your name?")
-name = input("Hello " + name + ", Glad to have you learning Python.")
+print("Hello " + name + ", Glad to have you learning Python.")
